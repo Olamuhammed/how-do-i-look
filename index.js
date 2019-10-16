384d0b7e1847d4326b56c49fdafc600767b99f3b
Olamuhammed <damolagunju@gmail.com>
2019-10-16 22:29:13 +0100

    i

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+function capturePic () {
+  window.alert('Picture has been taken')
+  var canvas = document.querySelector('#container00')
+  const video = document.querySelector('#videoElement')
+  const context = canvas.getContext('2d')
+  context.drawImage(video, 0, 0, canvas.width, canvas.height)
+  var picture = canvas.toDataURL()
+  console.log(picture)
+}
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
+
     .then(function (stream) {
       var video = document.querySelector('#videoElement')
       video.srcObject = stream
+      setTimeout(capturePic(), 5000)
     })
+
     .catch(function (err0r) {
       console.log('Something went wrong!')
     })

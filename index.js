f1f84438777c711bb96eb0b15e8dab31dc21de63
Olamuhammed <damolagunju@gmail.com>
2019-10-16 20:00:04 +0100

    another commit

---
+++ index.js
@@ -0,0 +1,9 @@
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream
+    })
+    .catch(function (err0r) {
+      console.log('Something went wrong!')
+    })
+}

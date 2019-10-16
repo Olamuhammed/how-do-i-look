8f7b434d4d10a9949fdd38923661b79137f8c6a2
Olamuhammed <damolagunju@gmail.com>
2019-10-16 20:04:38 +0100

    another one

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var video = document.querySelector('#videoElement')
+
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {

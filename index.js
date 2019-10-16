57d9978a3cffc2fec5fc94a9e564d81f6beb1caa
Olamuhammed <damolagunju@gmail.com>
2019-10-16 20:30:37 +0100

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var video = document.querySelector('#videoElement')
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
-      video.srcObject = stream
+      video.src = stream
     })
     .catch(function (err0r) {
       console.log('Something went wrong!')

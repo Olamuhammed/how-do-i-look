25b3cf6383d08611697504f0a682c4a193f102f7
Olamuhammed <damolagunju@gmail.com>
2019-10-17 11:58:48 +0100

    almost there

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('serviceWorker.js', {
+    scope: './'
+  })
+    .then((reg) => {
+      console.log('registered')
+    })
+    .catch((err) => {
+      console.log('there was an issue' + err)
+    })
+}
+
+let deferredPrompt
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Show the prompt
+  deferredPrompt.prompt()
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt')
+      } else {
+        console.log('User dismissed the A2HS prompt')
+      }
+      deferredPrompt = null
+    })
+})
+
 function capturePic () {
   // window.alert('Picture has been taken')
   var canvas = document.querySelector('#container')
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,39 @@
 
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceWorker.js')
-    .then((reg) => {
-      console.log('registered')
-    })
-    .catch((err) => {
-      console.log('there was an issue' + err)
-    })
-}
+var cacheName = 'v1'
+var filesTocache = [
+  'index.html',
+  'index.css',
+  'index.js'
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        cache.addAll(filesTocache)
+        console.log(filesTocache)
+      })
+      .then(() => self.skipWaiting())
+  )
+  console.log('service worker installed')
+})
+
+self.addEventListener('activate', (e) => {
+  console.log('serviceWorker activated')
+})
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches
+          .open(cacheName)
+          .then(cache => {
+            cache.put(e.request, clone)
+          })
+        return res
+      }).catch(err => console.log(err).then(res => res))
+  )
+})
